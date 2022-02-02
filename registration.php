0b0f2b8a99d5023d3beaa75a2ca6db9eee1d9229
Jared Blalock <jared.blalock@pulse-global.com>
2022-02-02 07:34:20 -0600

    Updates to initial module

--- registration.php
+++ registration.php
@@ -5,4 +5,8 @@
  */
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Physis_PlzaPay', __DIR__);
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Physis_PlzaPay',
+    __DIR__
+);
